00d980cbde788211adfd51eee8820c4b80e3a660
Felix <ilefotivff@gmail.com>
2014-01-17 11:00:50 +0100

    test

---
+++ src/main.java
@@ -0,0 +1,30 @@
+import org.terrier.structures.Index;
+import org.terrier.utility.ApplicationSetup;
+
+/**
+ * 
+ * @author apple_develop
+ * 
+ * Get QRELs
+ * Get Lexicon from DirInd
+ * 
+ * --- TRAINING PHASE ---
+ * Classificator: SVM, Naive Bayes
+ * SVM -> full cycle exp
+ * NB -> full cycle exp
+ * Generate 3 models
+ * 		Opinion (Pos+Neg+Mix), Positive, Negative
+ * 
+ * --- TEST PHASE ---
+ * For each classificator
+ * 		For each model
+ * 			For each test set -> classificate!
+ * Check metrics: Accuracy, Precision, Recall, ecc...
+ * 
+ */
+
+public class main {
+
+	Index index = Index.createIndex(ApplicationSetup.TERRIER_INDEX_PATH,ApplicationSetup.TERRIER_INDEX_PREFIX);
+
+}
